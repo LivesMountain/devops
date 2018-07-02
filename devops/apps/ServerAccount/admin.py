ad5ada9fa7f66cbee2474bc4a0b6dc2d27a4be33
likun <likun@guoshengtianfeng.com>
2018-07-02 10:58:05 +0800

    完成666

--- devops/apps/ServerAccount/admin.py
+++ devops/apps/ServerAccount/admin.py
@@ -12,7 +12,7 @@ class ArticleAdmin(admin.ModelAdmin):
         for o in queryset.all():
             o.delete()
             s = requests.session()
-            url="http://127.0.0.1:8000/sendmail/api/?message=你的服务器：%s账号申请被拒绝&con=%s&header=请查收"%(o.ip,o.email)
+            url="http://127.0.0.1:8080/sendmail/api/?message=你的服务器：%s账号申请被拒绝&con=%s&header=请查收"%(o.ip,o.email)
             response = s.get(url=url)
 
     delete_selected.short_description = '我拒绝'
@@ -20,7 +20,7 @@ class ArticleAdmin(admin.ModelAdmin):
     def agree(self, request,queryset):
         queryset.all().update(check=1)
         for o in queryset.all():
-            url='http://127.0.0.1:8000/sendmail/api/?message=你的服务器：%s账号申请已通过\n账号会在1分钟内添加成功&con=%s&header=请查收'%(o.ip,o.email)
+            url='http://127.0.0.1:8080/sendmail/api/?message=你的服务器：%s账号申请已通过\n账号会在1分钟内添加成功&con=%s&header=请查收'%(o.ip,o.email)
             s = requests.session()
             response = s.get(url=url)
     agree.short_description = '我同意'
